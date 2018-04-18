68bece3b0d04bc22ae7fe1e73d4aebab7382ecc3
zxldoris <zxldoris@gmail.com>
2018-04-18 20:44:39 +0800

    jquery

---
+++ jquery/mouseEvent/script.js
@@ -0,0 +1,27 @@
+$(function () {
+    $("#click1").click(function () {
+        alert(1);
+    });
+    //hover里面有两个函数参数，第一个是进入标签，第二个是离开标签
+    $("#hover1").hover(function () {
+        alert("进入");
+    },function () {
+        alert("离开");
+    });
+    $("#input1").blur(function () {
+        alert("blur");
+    });
+    $("#input1").focus(function () {
+        alert("focus");
+    });
+    // 推荐
+    $("#input3").keyup(function () {
+        $("#target").text($(this.val()))
+    });
+    $("#input4").keydown(function () {
+        $("#target").text($(this.val()))
+    });
+    $("#input5").keypress(function () {
+        $("#target").text($(this.val()))
+    });
+});
